a2fe85ee8b022161793335a3ffb9bbbbdd779def
parnika-singh11 <parnika110307@gmail.com>
2025-10-10 16:39:16 -0700

    feat: image formatting
    feat: added images
    
    feat: added css
    
    feat: added image style
    
    feat: added js
    
    feat: added links
    
    adding blog links
    
    feat adding content to html files

--- src/blog.js
+++ src/blog.js
@@ -1,18 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var Blogs = [
     {
         title: "Hello, world!",
         data: "This is my blog!",
         description: "This is my first blog!",
-        image: "",
-        imageAlt: "",
+        image: "/Users/parnikasingh/Downloads/images.png",
+        imageAlt: "helloworld",
         slug: "my-blog-1",
     },
     {
         title: "Hack4Impact bootcamp",
         data: "this is my second blog!",
         description: "Typescript is fun!",
-        image: "",
-        imageAlt: "",
+        image: "/Users/parnikasingh/Downloads/Typescript.ApdKzZht.png",
+        imageAlt: "typescript",
         slug: "my-blog-2",
     },
 ];
@@ -21,6 +23,8 @@ var blog;
 function printBlogs() {
     for (var _i = 0, Blogs_1 = Blogs; _i < Blogs_1.length; _i++) {
         blog = Blogs_1[_i];
+        var link = document.createElement("a");
+        link.href = "blogs/".concat(blog.slug, ".html");
         var d = document.createElement("div");
         var title = document.createElement("h1");
         title.textContent = blog.title;
@@ -29,10 +33,12 @@ function printBlogs() {
         var image = document.createElement("img");
         image.src = blog.image;
         image.alt = blog.imageAlt;
+        image.classList.add("blog-image");
         d.appendChild(title);
         d.appendChild(description);
         d.appendChild(image);
-        blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(d);
+        link.appendChild(d);
+        blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(link);
     }
 }
 printBlogs();
--- src/blog.ts
+++ src/blog.ts
@@ -1,3 +1,5 @@
+import ts = require("typescript");
+
 type Blog = {
     title: string;
     data: string;
@@ -11,16 +13,16 @@ const Blogs: Blog[] = [
         title: "Hello, world!",
         data: "This is my blog!",
         description: "This is my first blog!",
-        image: "",
-        imageAlt: "",
+        image: "/Users/parnikasingh/Downloads/images.png",
+        imageAlt: "helloworld",
         slug: "my-blog-1",
     },
     {
         title: "Hack4Impact bootcamp",
         data: "this is my second blog!",
         description: "Typescript is fun!",
-        image: "",
-        imageAlt: "",
+        image: "/Users/parnikasingh/Downloads/Typescript.ApdKzZht.png",
+        imageAlt: "typescript",
         slug: "my-blog-2",
     },
 
@@ -29,6 +31,9 @@ const blogContainer = document.getElementById('blog-container');
 var blog:any;
 function printBlogs() {
     for(blog of Blogs){
+        
+        const link = document.createElement("a");
+        link.href = `blogs/${blog.slug}.html`;
         const d = document.createElement("div");
 
         const title = document.createElement("h1");
@@ -39,7 +44,8 @@ function printBlogs() {
 
         const image = document.createElement("img");
         image.src = blog.image;
-        image.alt = blog.imageAlt
+        image.alt = blog.imageAlt;
+        image.classList.add("blog-image"); 
 
 
 
@@ -48,7 +54,8 @@ function printBlogs() {
         d.appendChild(description);
         d.appendChild(image);
 
-        blogContainer?.appendChild(d);
+        link.appendChild(d);
+        blogContainer?.appendChild(link);
 
     }
 
