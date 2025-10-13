1abd55fcf129845284b1f13b228e7741dfac5fba
parnika-singh11 <parnika110307@gmail.com>
2025-10-13 16:56:50 -0700

    feat: troubleshooting
    feat: troubleshooting praying
    
    i have no idea
    
    feat:
    
    feat:
    
    feat: idk
    
    feat
    
    please work
    
    feat:
    
    feat added log
    
    feat
    
    feat
    
    feat: .js indtead of .ts
    
    added folder for images
    
    feat: added images
    
    feat: consoles
    
    feat:edited links display
    
    feat: added nav list and div
    
    feat:added script and list

--- src/blog.js
+++ src/blog.js
@@ -1,26 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var Blogs = [
     {
-        title: "Hello, world!",
+        title: "Blog 1: Hello, world!",
         data: "This is my blog!",
         description: "This is my first blog!",
-        image: "/Users/parnikasingh/Downloads/images.png",
+        image: "images/images.png",
         imageAlt: "helloworld",
         slug: "my-blog-1",
     },
     {
-        title: "Hack4Impact bootcamp",
+        title: "Blog 2: Hack4Impact bootcamp",
         data: "this is my second blog!",
         description: "Typescript is fun!",
-        image: "/Users/parnikasingh/Downloads/Typescript.ApdKzZht.png",
+        image: "images/Typescript.ApdKzZht.png",
         imageAlt: "typescript",
         slug: "my-blog-2",
     },
 ];
 var blogContainer = document.getElementById('blog-container');
+console.log("blog container", blogContainer);
 var blog;
 function printBlogs() {
+    console.log("this worked");
     for (var _i = 0, Blogs_1 = Blogs; _i < Blogs_1.length; _i++) {
         blog = Blogs_1[_i];
         var link = document.createElement("a");
@@ -28,15 +28,14 @@ function printBlogs() {
         var d = document.createElement("div");
         var title = document.createElement("h1");
         title.textContent = blog.title;
-        var description = document.createElement("p");
+        /*const description= document.createElement("p");
         description.textContent = blog.description;
-        var image = document.createElement("img");
+
+        const image = document.createElement("img");
         image.src = blog.image;
         image.alt = blog.imageAlt;
-        image.classList.add("blog-image");
+        image.classList.add("blog-image"); */
         d.appendChild(title);
-        d.appendChild(description);
-        d.appendChild(image);
         link.appendChild(d);
         blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(link);
     }
--- src/blog.ts
+++ src/blog.ts
@@ -1,4 +1,4 @@
-import ts = require("typescript");
+
 
 type Blog = {
     title: string;
@@ -10,26 +10,29 @@ type Blog = {
 };
 const Blogs: Blog[] = [
     {
-        title: "Hello, world!",
+        title: "Blog 1: Hello, world!",
         data: "This is my blog!",
         description: "This is my first blog!",
-        image: "/Users/parnikasingh/Downloads/images.png",
+        image: "images/images.png",
         imageAlt: "helloworld",
         slug: "my-blog-1",
     },
     {
-        title: "Hack4Impact bootcamp",
+        title: "Blog 2: Hack4Impact bootcamp",
         data: "this is my second blog!",
         description: "Typescript is fun!",
-        image: "/Users/parnikasingh/Downloads/Typescript.ApdKzZht.png",
+        image: "images/Typescript.ApdKzZht.png",
         imageAlt: "typescript",
         slug: "my-blog-2",
     },
 
 ];
 const blogContainer = document.getElementById('blog-container');
+console.log("blog container",blogContainer);
+
 var blog:any;
 function printBlogs() {
+    console.log("this worked");
     for(blog of Blogs){
         
         const link = document.createElement("a");
@@ -39,20 +42,19 @@ function printBlogs() {
         const title = document.createElement("h1");
         title.textContent = blog.title;
 
-        const description= document.createElement("p");
+        /*const description= document.createElement("p");
         description.textContent = blog.description;
 
         const image = document.createElement("img");
         image.src = blog.image;
         image.alt = blog.imageAlt;
-        image.classList.add("blog-image"); 
+        image.classList.add("blog-image"); */
 
 
 
 
         d.appendChild(title);
-        d.appendChild(description);
-        d.appendChild(image);
+    
 
         link.appendChild(d);
         blogContainer?.appendChild(link);
